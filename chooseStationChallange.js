ec04337e2724508c69c7d758f574dd235679e936
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-03-06 03:43:38 +0000

    StationChallange

---
+++ chooseStationChallange.js
@@ -0,0 +1,23 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+/*
+1. Must have a capacity of at least 20
+2. Be a school or community centre
+
+The chooseStations function should return the array:
+  ['Bright Lights Elementary', 'Moose Mountain Community Centre']
+*/
+
+function chooseStations (stations) {
+  const goodStations = [];
+  for (const station of stations) {
+    if (station[1] >= 20 && (station[2] === 'school' || station[2] === 'community centre')){
+      goodStations.push(station[0]);
+    }
+  }
+  return goodStations;
+}
