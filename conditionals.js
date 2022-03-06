95aa7585f321747a9887c3d5820dc67e2eab19e3
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-03-06 03:09:29 +0000

    isOdd function

--- conditionals.js
+++ conditionals.js
@@ -1,30 +1,16 @@
-const temperature = 12;
+const whichSchool  = function (age) {
+  if (age < 13){
+    return "Elementary School";
+  } else if (age <= 18 && age >= 13) {
+    return "Secondary School";
+  }
+  else return "Lighthouse Labs";
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
 }
-
-console.log("Now you're ready to go outside!");
-
-
-// Logical operation
-const isCitizen = true;
-const age = 26;
-
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
-
-
-
-if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
-}
-
-
-
-
+  
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
