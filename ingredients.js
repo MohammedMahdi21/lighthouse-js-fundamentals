a1fcdfb3511f79ddcdecdc3d6ad8caaf32dc633e
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-03-07 04:34:26 +0000

    ingredients challange

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0
+while (i < ingredients.length){
+console.log(ingredients[i]);
+i++
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (let y = 0; y< ingredients.length; y++){
+  console.log(ingredients[y]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+let z = 7
+while (z >= 0){
+  console.log(ingredients[z]);
+  z--
+  }
