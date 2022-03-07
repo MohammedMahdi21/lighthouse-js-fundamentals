340f0b0571e4902d92d306d91044a28e5fb8c69d
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-03-07 04:21:51 +0000

    ice-cream challange

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+
+let y = 0;
+for (let i = 0; i < iceCreamFlavours.length; i++) {
+
+  iceCreamFlavours[i];
+  y += 1;
+}
+console.log(y);
